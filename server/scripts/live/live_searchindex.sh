7700f248de7b19a0d0e446833c45cf17b1632f89
Jeremy Greenfield <jsgreenf@gmail.com>
2012-06-08 15:56:29 -0400

    Removed server

--- server/scripts/live/live_searchindex.sh
+++
@@ -1,6 +0,0 @@
-# rebuild search index
-echo "y\n" | python /srv/live/lovegov/live_manage.py rebuild_index
-# reset permissions on search index
-chmod -R 770 /srv/live/lovegov/beta/modernpolitics/search_indexes
-chgrp -R access /srv/live/lovegov/beta/modernpolitics/search_indexes
-
