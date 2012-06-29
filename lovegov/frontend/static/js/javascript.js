4f09f570d1b1a7db36d5de8fe2e2e03adcd38262
CLAY <clay@lovegov.com>
2012-06-29 12:53:53 -0400

    vimeo video change

--- lovegov/frontend/static/js/javascript.js
+++ lovegov/frontend/static/js/javascript.js
@@ -3419,6 +3419,7 @@ function refreshHistogramData(data) {
 
         bar.children('.red_bar').css("background-color",data.color);
         $('.histogram-footer').css("background-color",data.color);
+        $('.histogram-wrapper').css("background-color",data.color);
 
         var num = bar.data('num') + item.num;
         bar.data('num', num);
