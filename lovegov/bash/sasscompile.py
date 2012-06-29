08613f09aa75345b7e44a4dce0330395d0ca6526
Max Fowler <max_fowler@brown.edu>
2012-06-29 17:22:29 -0400

    recursive sass compile

--- lovegov/bash/sasscompile.py
+++ lovegov/bash/sasscompile.py
@@ -4,12 +4,20 @@ import os
 css = PROJECT_PATH + '/frontend/static/css'
 compiled = css + '/compiled'
 scss = css + '/scss'
-scss_files = os.listdir(scss)
 
-for f in scss_files:
-    splitted = f.split(".")
-    name = splitted[0]
-    scss_path = scss + '/' + f
-    compiled_path = compiled + '/' + name + ".css"
-    command = "sass " + scss_path + " " + compiled_path
-    os.system(command)
+def compileFolder(relative_path):
+    scss_files = os.listdir(scss + relative_path)
+    for f in scss_files:
+        splitted = f.split(".")
+        if len(splitted) == 2:                # then file
+            name = splitted[0]
+            scss_path = scss + relative_path + '/' + f
+            compiled_path = compiled + relative_path + '/' + name + ".css"
+            command = "sass " + scss_path + " " + compiled_path
+            os.system(command)
+        else:                               # then folder
+            directory_relative_path = relative_path + "/" + splitted[0]
+            print directory_relative_path
+            compileFolder(directory_relative_path)
+
+compileFolder('')
