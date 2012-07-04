0f538510ccdd4232996ff604683ac22e1c2de671
jvkoh <jonathanvkoh@gmail.com>
2012-07-04 14:23:43 -0400

    fixing stuff and working on counting things

--- lovegov/modernpolitics/defaults.py
+++ lovegov/modernpolitics/defaults.py
@@ -859,6 +859,13 @@ def initializeLegislation():
 
             total+=1
 
+def countLegislation():
+    count = 0
+    for num in range(109,113):
+        filePath = '/data/govtrack/' + str(num) + "/bills/"
+        count += filecount(filePath)
+    return count
+
 def initializeLegislationAmendments():
     for num in range(109,113):
         filePath = '/data/govtrack/' + str(num) + "/bills.amdt/"
@@ -879,6 +886,13 @@ def initializeLegislationAmendments():
                     traceback.print_exc()
                 count+=1
 
+def countLegislationAmendments():
+    count = 0
+    for num in range(109,113):
+        filePath = '/data/govtrack/' + str(num) + "/bills.amdt/"
+        count += filecount(filePath)
+    return count
+
 # Initialize Voting Records
 def initializeVotingRecord():
     for num in range(109,113):
@@ -895,6 +909,14 @@ def initializeVotingRecord():
             except:
                 print "ERROR parsing " + infile + " " + str(count) + '/' + str(fileCount)
                 traceback.print_exc()
+
+def countVotingRecords():
+    count = 0
+    for num in range(109,113):
+        filePath = '/data/govtrack/' + str(num) + "/rolls/"
+        count += filecount(filePath)
+    return count
+
 
 #-------------------------------------------------------------------------------------------------------------------
 # Wrapper for create user helper which makes a superuser.
