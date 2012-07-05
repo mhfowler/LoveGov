78ac1d4fc959e68b5609be1752a0ea7a3933eea5
CLAY <clay@lovegov.com>
2012-07-05 00:09:14 -0400

    qaweb sexy

--- lovegov/frontend/views.py
+++ lovegov/frontend/views.py
@@ -309,8 +309,9 @@ def getUserWebResponsesJSON(request,vals={},webCompare=False):
             answerArray.append(answer)
         toAddquestion = {'id':question.id,'text':question.question_text,'answers':answerArray,'user_explanation':"",'childrenData':[]}
         if len(response) > 0 : toAddquestion['user_explanation'] = response[0].userresponse.explanation
+
         if not webCompare and len(response) > 0: toAddquestion['security'] = response[0].userresponse.privacy
-        else: toAddQuestion['security'] = ""
+        else: toAddquestion['security'] = ""
         questionsArray[topic_text].append(toAddquestion)
     vals['questionsArray'] = json.dumps(questionsArray)
 
