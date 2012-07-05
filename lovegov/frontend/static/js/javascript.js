100c722429aac3c5801df72c0a41d3b3ddd640cb
Jeremy Greenfield <jsgreenf@gmail.com>
2012-07-05 17:15:32 -0400

    Footer width and inline edit html

--- lovegov/frontend/static/js/javascript.js
+++ lovegov/frontend/static/js/javascript.js
@@ -439,7 +439,7 @@ function editContent(c_id,info,edit_div)
             var obj = eval('(' + data + ')');
             if( obj.success )
             {
-                edit_div.text(obj.value);
+                edit_div.html(obj.value);
                 edit_div.show();
             }
         },
@@ -4066,6 +4066,7 @@ function swapInHover(div) {
 function bindChangeContentPrivacy() {
 
     $('div.change-privacy').bindOnce('click', function() {
+        alert('clicky');
         var content_id = $(this).data('content_id');
         var meDiv = $(this);
         $(this).tooltip('hide');
