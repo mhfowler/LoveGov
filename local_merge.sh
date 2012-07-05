0a5b22dfafe907bef4d4210ed1b2e224031a3e0b
Max Fowler <max_fowler@brown.edu>
2012-07-05 04:21:08 -0400

    merge.

--- local_merge.sh
+++ local_merge.sh
@@ -2,13 +2,13 @@ LG="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $LG
 git checkout dev
 git add -i
-git commit -m "merge."
+git commit -a -m "merge."
 git push origin dev
 git checkout master
 git merge dev
 git pull origin master
 git add -i
-git commit -m "merge."
+git commit -a -m "merge."
 git push origin master
 git checkout dev
 cd -
