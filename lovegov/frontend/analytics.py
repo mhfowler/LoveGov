b6c58c7d90ffb0177b3ec11674c4752599ef293e
Max Fowler <max_fowler@brown.edu>
2012-07-09 14:54:40 -0400

    woo

--- lovegov/frontend/analytics.py
+++ lovegov/frontend/analytics.py
@@ -41,7 +41,7 @@ def userActivity(user):
 
     pa = PageAccess.objects.filter(user=user).order_by("when")
 
-    when = datetime.MINYEAR
+    when = datetime.date.min
     to_return = "User Summary for " + user.get_name() + ": \n"
 
     for x in pa:
