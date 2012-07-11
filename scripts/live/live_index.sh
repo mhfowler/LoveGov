2e61dff595dd7ac507a6a22db348d5867fd7f138
LoveGov server <love@lovegov.com>
2012-07-11 02:23:46 -0400

    server

--- scripts/live/live_index.sh
+++ scripts/live/live_index.sh
@@ -1,6 +1,6 @@
 # rebuild search index
 echo "y\n" | python /srv/live/lovegov/live_manage.py rebuild_index
 # reset permissions on search index
-chmod -R 770 /srv/live/lovegov/modernpolitics/search_indexes
-chgrp -R access /srv/live/lovegov/modernpolitics/search_indexes
+sudo chmod -R 770 /srv/live/lovegov/modernpolitics/search_indexes
+sudo chgrp -R access /srv/live/lovegov/modernpolitics/search_indexes
 
