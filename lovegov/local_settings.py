67611a6bdc0e57532016af37fec980d9d4defdcc
Jeremy Greenfield <jsgreenf@gmail.com>
2012-07-13 11:08:56 -0400

    Local database to mysql

--- lovegov/local_settings.py
+++ lovegov/local_settings.py
@@ -7,22 +7,13 @@ PROJECT_PATH = os.path.dirname(__file__)
 
 DATABASES = {
     'default': {
-        'ENGINE':   'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.django.db.backends.dummy
-        'NAME':     os.path.join(PROJECT_PATH, 'db/local.db'),
-        'USER':      '',   # Not used with sqlite3.
+        'ENGINE':   'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.django.db.backends.dummy
+        'NAME':     'localmirror',
+        'USER':      'root',   # Not used with sqlite3.
         'PASSWORD':  '',       # Not used with sqlite3.
         'HOST':     '',      # Set to empty string for localhost. Not used with sqlite3.
         'PORT':    '',        # Set to empty string for default. Not used with sqlite3.
     },
-    'qaweb': {
-        'ENGINE':   'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.django.db.backends.dummy
-        'NAME': os.path.join(PROJECT_PATH, 'db/qaweb12').replace('\\','/'),   # Or path to database file if using sqlite3.
-        #'NAME': 'novavote',
-        'USER':      '',   # Not used with sqlite3.
-        'PASSWORD':  '',       # Not used with sqlite3.
-        'HOST':     '',      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT':    '',        # Set to empty string for default. Not used with sqlite3.
-    }
 }
 
 
