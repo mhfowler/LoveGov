9ac4abd0e40055d544af2be448c1028a45ffb3a3
Max Fowler <max_fowler@brown.edu>
2012-07-24 15:52:48 -0400

    yup

--- local_env.sh
+++ local_env.sh
@@ -6,3 +6,6 @@ echo 'whereami: '$SCRIPTPATH
 export PYTHONPATH=${PYTHONPATH}:$SCRIPTPATH
 export PYTHONPATH=${PYTHONPATH}:$SCRIPTPATH/lovegov
 export DJANGO_SETTINGS_MODULE=lovegov.local_settings
+
+# run commands if given
+$1 $2 $3 $4 $5 $6 $7 $8
