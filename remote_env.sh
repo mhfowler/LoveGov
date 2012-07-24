8abc4891fb19b78ef1fcb2077fae14f3b6510ea4
Max Fowler <max_fowler@brown.edu>
2012-07-24 15:34:30 -0400

    maybe

---
+++ remote_env.sh
@@ -0,0 +1,8 @@
+# absolute path to this script
+SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo 'whereami: '$SCRIPTPATH
+
+#path stuff
+export PYTHONPATH=${PYTHONPATH}:$SCRIPTPATH
+export PYTHONPATH=${PYTHONPATH}:$SCRIPTPATH/lovegov
+export DJANGO_SETTINGS_MODULE=lovegov.devremote_settings
