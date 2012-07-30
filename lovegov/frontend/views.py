71e4141faef3b5ef7c79797a55de97c4f5b348a8
Edric Lescouflair <stancompedu@yahoo.com>
2012-07-30 17:29:33 -0400

    legislative trial changes3

--- lovegov/frontend/views.py
+++ lovegov/frontend/views.py
@@ -882,7 +882,6 @@ def legislation(request, session=None, type=None, number=None, vals={}):
         vals['leg'] = leg
     return renderToResponseCSRF(template='site/pages/legislation/legislation-view.html', vals=vals, request=request)
 
-def legislationFilter():
 
 
 #-----------------------------------------------------------------------------------------------------------------------
