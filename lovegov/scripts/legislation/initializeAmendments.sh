93cc7b8a0aceb57c7b2e2ada3e7f657603fff4f1
jvkoh <jonathanvkoh@gmail.com>
2012-08-01 15:40:41 -0400

    ahhhhh do it now

--- lovegov/scripts/legislation/initializeAmendments.sh
+++ lovegov/scripts/legislation/initializeAmendments.sh
@@ -1,3 +1,1 @@
-SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 nohup ./initializeAmendments.py > /log/legislation/amendments_log.txt &
--- lovegov/scripts/legislation/initializeCongressVotes.sh
+++ lovegov/scripts/legislation/initializeCongressVotes.sh
@@ -1,4 +1,2 @@
-SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 nohup ./initializeCongressVotes.py > /log/legislation/votes_log.txt &
 
