ddb596336c662e167fade7109bd5e9c32bc10842
jvkoh <jonathanvkoh@gmail.com>
2012-08-01 10:22:49 -0400

    fixing scripts

--- lovegov/scripts/legislation/initializeLegislation.sh
+++ lovegov/scripts/legislation/initializeLegislation.sh
@@ -1,4 +1,4 @@
-HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-nohup $HERE/initializeLegislation.py > /log/legislation/legislation_log.txt &
+nohup $SCRIPTPATH/initializeLegislation.py > /log/legislation/legislation_log.txt &
 
