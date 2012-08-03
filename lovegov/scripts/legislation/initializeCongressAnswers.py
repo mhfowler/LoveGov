d6fc7aac6b5bbf2dcbc0b40716c6a55d0689664d
jvkoh <jonathanvkoh@gmail.com>
2012-08-03 15:41:25 -0400

    why doesn't the initializecongressanswers script work

--- lovegov/scripts/legislation/initializeCongressAnswers.py
+++ lovegov/scripts/legislation/initializeCongressAnswers.py
@@ -1,11 +1,8 @@
-
-
-
 if __name__ == "__main__":
     from lovegov.scripts.alpha import scriptCreateCongressAnswers
     from pprint import pprint
 
-    print "================= scriptCreateCongressAnswers() ======================="
+    print "================= scriptCreateCongressAnswers ======================="
     print '======================================================================='
     metrics = scriptCreateCongressAnswers()
     print "======================= Metrics ========================"
