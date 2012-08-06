1522ac0aa8cd78695dd1ad08130c3f373ac3441d
Max Fowler <max_fowler@brown.edu>
2012-08-06 16:31:01 -0400

    in the middle of something

--- local_aliases.sh
+++ local_aliases.sh
@@ -1,5 +1,7 @@
 # absolute path to the directory of this script
 LG="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LG=$(dirname $0)
+echo "lg: $LG"
 
 # aliases
 alias cmdcreate='vi $LG/local_aliases.sh'
--- local_reset.sh
+++ local_reset.sh
@@ -1,6 +1,5 @@
 #absolute path to this script
 SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo 'whereami: '$SCRIPTPATH
 
 # cleardb
 rm $SCRIPTPATH/lovegov/db/local.db
