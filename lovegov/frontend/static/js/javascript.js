dbec12a3ab5889a1a756275ea89f1e9a72f9d7bb
Edric Lescouflair <stancompedu@yahoo.com>
2012-08-13 18:44:28 -0400

    new legislation changes8

--- lovegov/frontend/static/js/javascript.js
+++ lovegov/frontend/static/js/javascript.js
@@ -5145,12 +5145,6 @@ bind('#create-account', 'click', null, function(event)
     $('#register-div').show();
 });
 
-/***********************************************************************************************************************
- *
- *      ~Legislation
- *
- **********************************************************************************************************************/
-
 bind(".create_account_button", 'click' ,null, function(event) {
     event.preventDefault();
     $("#create-account-div").hide();
@@ -5425,4 +5419,43 @@ function getModal(modal_name,data)
 
 
 
+/***********************************************************************************************************************
+ *
+ *      ~Legislation
+ *
+ **********************************************************************************************************************/
 
+
+$(document).ready(function () {
+    var showChar = 100;
+    var ellipsestext = "...";
+    var moretext = "read more";
+    var lesstext = "less";
+    $('.long_text').each(function() {
+        var content = $(this).html();
+
+        if(content.length > showChar) {
+
+            var c = content.substr(0, showChar);
+            var h = content.substr(showChar-1, content.length - showChar);
+
+            var html = c + '<span class="moreellipses">' + ellipsestext + '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+
+            $(this).html(html);
+        }
+
+    });
+
+    $(".morelink").click(function(){
+        if($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+});
