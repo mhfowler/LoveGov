6116625d596070eb3c54dcdc2c9275aea793d4ce
Max Fowler <max_fowler@brown.edu>
2012-08-15 13:25:06 -0400

    woop woop

--- lovegov/scripts/sendEmail.py
+++ lovegov/scripts/sendEmail.py
@@ -4,13 +4,6 @@ from django.core.mail import send_mail
 
 print "*** SENDING SPECIAL EMAIL ***"
 
-if len(sys.argv) > 1:
-    email = sys.argv[1]
-    print "sending to: " + email
-    email_recipients = [email]
-else:
-    email_recipients = DAILY_SUMMARY_EMAILS
-
 users = UserProfile.objects.filter(developer=True)
 
 for u in users:
@@ -18,7 +11,7 @@ for u in users:
     context = Context(vals)
     template = loader.get_template('emails/vicepresidential.html')
     email_html = template.render(context)
-    email_recipient = [u.email]
+    email_recipients = [u.email]
     sendHTMLEmail(subject="LoveGov VicePresidential Match", email_html=email_html,
     email_sender="team@lovegov.com", email_recipients=email_recipients)
     print "sent: " + u.get_name()
