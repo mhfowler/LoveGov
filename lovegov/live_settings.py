afc553569d63fa9cf0a338560e41b4f5a0c60937
Max Fowler <max_fowler@brown.edu>
2012-09-01 12:09:49 -0400

    live settings

--- lovegov/live_settings.py
+++ lovegov/live_settings.py
@@ -19,11 +19,8 @@ DATABASES = {
 }
 
 STATIC_ROOT = '/static/live/'
-
-MEDIA_ROOT = '/media/live/'
-
+MEDIA_ROOT = '/media/dlive/'
 LOG_ROOT = "/log/live/"
-
 LOGGING = base_settings.setLogging(LOG_ROOT)
 
 ############################### EVERYTHING BELOW THE SAME ##############################################################
@@ -35,7 +32,10 @@ LOGGING = base_settings.setLogging(LOG_ROOT)
 
 ROOT_URLCONF = base_settings.ROOT_URLCONF
 
-INSTALLED_APPS = base_settings.INSTALLED_APPS.__add__(('south',))
+INSTALLED_APPS = base_settings.INSTALLED_APPS.__add__(('south',
+                                                       'storages',
+                                                       's3_folder_storage',
+                                                       'djcelery',))
 
 MIDDLEWARE_CLASSES = base_settings.MIDDLEWARE_CLASSES
 
@@ -44,20 +44,42 @@ MIDDLEWARE_CLASSES = base_settings.MIDDLEWARE_CLASSES
 #
 ########################################################################################################################
 
-# URL prefix for static files.
-STATIC_URL = base_settings.STATIC_URL
+USE_S3 = False
 
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a trailing slash.
-MEDIA_URL =  base_settings.MEDIA_URL
+if USE_S3:
+    import s3_configuration
+    AWS_ACCESS_KEY_ID = s3_configuration.AWS_ACCESS_KEY_ID
+    AWS_SECRET_ACCESS_KEY = s3_configuration.AWS_SECRET_ACCESS_KEY
+    AWS_STORAGE_BUCKET_NAME = s3_configuration.AWS_STORAGE_BUCKET_NAME
 
-# URL prefix for admin static files -- CSS, JavaScript and images. Make sure to use a trailing slash.
-ADMIN_MEDIA_PREFIX = base_settings.ADMIN_MEDIA_PREFIX
+    DEFAULT_FILE_STORAGE = 's3_folder_storage.s3.DefaultStorage'
+    STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
 
-# Additional locations of static files
-STATICFILES_DIRS = base_settings.STATICFILES_DIRS
+    DEFAULT_S3_PATH = "media"
+    STATIC_S3_PATH = "static"
 
-# List of finder classes that know how to find static files in various locations.
-STATICFILES_FINDERS = base_settings.STATICFILES_FINDERS
+    MEDIA_ROOT = '/%s/' % DEFAULT_S3_PATH
+    MEDIA_URL = 'https://%s.s3.amazonaws.com/media' % AWS_STORAGE_BUCKET_NAME
+    STATIC_ROOT = "/%s/" % STATIC_S3_PATH
+    STATIC_URL = 'https://%s.s3.amazonaws.com/static' % AWS_STORAGE_BUCKET_NAME
+    ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+
+    STATICFILES_DIRS = base_settings.STATICFILES_DIRS
+    STATICFILES_FINDERS = base_settings.STATICFILES_FINDERS
+
+    from compressor_settings import *
+    COMPRESS_ENABLED = False
+
+else:
+    # URL prefix for static files.
+    STATIC_URL = '/static'
+    MEDIA_URL =  base_settings.MEDIA_URL
+    ADMIN_MEDIA_PREFIX = base_settings.ADMIN_MEDIA_PREFIX
+    STATICFILES_DIRS = base_settings.STATICFILES_DIRS
+    STATICFILES_FINDERS = base_settings.STATICFILES_FINDERS
+    from compressor_settings import *
+    COMPRESS_URL = "/fake/"
+    COMPRESS_ENABLED = False
 
 ########################################################################################################################
 #    templates
