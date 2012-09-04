c694628fc674c7bc84d7e6c6a0e571a4603cd1ee
Max Fowler <max_fowler@brown.edu>
2012-09-03 20:50:23 -0400

    les go

--- lovegov/scripts/migration/group_aliases.py
+++ lovegov/scripts/migration/group_aliases.py
@@ -1,5 +1,6 @@
-from lovegov.frontend.views import *
-from lovegov.modernpolitics.recalculate import *
+#!/usr/bin/env python
 
-print "recalculating group aliases!"
-recalculateGroupAliases()
+if __name__ == "__main__":
+    from lovegov.modernpolitics.recalculate import recalculateGroupAliases
+    print "recalculating group aliases!"
+    recalculateGroupAliases()
