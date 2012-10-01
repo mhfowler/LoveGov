81052761fc16bd8bb8d0d278a52529d4923ccde9
Jeremy Greenfield <jsgreenf@gmail.com>
2012-10-01 12:44:51 -0400

    professor emails

--- lovegov/scripts/email/sendLaunchEmail.py
+++ lovegov/scripts/email/sendLaunchEmail.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
 if __name__ == "__main__":
-    from lovegov.modernpolitics.send_email import sendGroupGeneralInviteEmail
+    from lovegov.modernpolitics.send_email import sendProfessorInviteEmail
 
     print "================= sendStudentGroupInviteEmail() ==================="
     print '============================================================='
-    total_sent = sendGroupGeneralInviteEmail('frontend/excel/AcademiaBundle_MA.xls', 1)
+    total_sent = sendProfessorInviteEmail('frontend/excel/AcademiaBundle_MA.xls', 2)
     print "=================== Total Sent ======================"
     print("#: " + str(total_sent))
 
