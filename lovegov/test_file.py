f732df2dbe6ce23a7f0ff0cd6d15909d2b11c0a5
Max Fowler <max_fowler@brown.edu>
2012-10-16 14:35:02 -0400

    metrics

--- lovegov/test_file.py
+++ lovegov/test_file.py
@@ -1,14 +1,12 @@
-""" sends emails to all developers describing usage of the site that day."""
-
 from lovegov.frontend.analytics import *
-
-#time_start = datetime.datetime(year=2012, month=10, day=11, hour=6)
-#time_end = datetime.datetime.now()
-#
-#analyzeCookieData(time_start, time_end)
-
 from lovegov.frontend.views import *
-from lovegov.scripts.alpha import scriptCreateCongressAnswers
 
-m = getUser("Max Fowler")
-sendWeeklyDigestEmail(m)
+start_date = datetime.datetime(year=2012, month=4, day=10, hour=6)
+end_date = datetime.datetime.now()
+
+delta = datetime.timedelta(days=1)
+time_tuples = getTimeTuplesByDelta(start_date, end_date, delta)
+
+output_file = os.path.join(PROJECT_PATH, 'logging/metrics/registration_conversion.xls')
+
+registrationConversionAnalytics(time_tuples, output_file)
