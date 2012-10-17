9e563737f5b4330330df34a979e0048ade1300a3
Max Fowler <max_fowler@brown.edu>
2012-10-17 18:04:42 -0400

    completed tasks test run

--- lovegov/scripts/analytics/completed_tasks.py
+++ lovegov/scripts/analytics/completed_tasks.py
@@ -8,12 +8,12 @@ if __name__ == "__main__":
 
     updateUserAnalyticsData()
 
-    start_date = datetime.datetime(year=2012, month=9, day=5, hour=6)
+    start_date = datetime.datetime(year=2012, month=4, day=10, hour=6)
     end_date = datetime.datetime.now()
 
-    delta = datetime.timedelta(days=1)
+    delta = datetime.timedelta(days=7)
     time_tuples = getTimeTuplesByDelta(start_date, end_date, delta)
 
-    output_file = os.path.join(PROJECT_PATH, 'logging/metrics/completed_tasks_daily.xls')
+    output_file = os.path.join(PROJECT_PATH, 'logging/metrics/completed_tasks_weekly.xls')
 
     completedTasksAnalytics(time_tuples, output_file)
--- lovegov/test_file.py
+++ lovegov/test_file.py
@@ -1,16 +1,12 @@
 from lovegov.frontend.analytics import *
 from lovegov.frontend.views import *
 
-initializeAnalyticsTasks()
-
-updateUserAnalyticsData()
-
-start_date = datetime.datetime(year=2012, month=10, day=1, hour=6)
+start_date = datetime.datetime(year=2012, month=9, day=25, hour=6)
 end_date = datetime.datetime.now()
 
 delta = datetime.timedelta(days=1)
 time_tuples = getTimeTuplesByDelta(start_date, end_date, delta)
 
-output_file = os.path.join(PROJECT_PATH, 'logging/metrics/completed_tasks_daily.xls')
+output_file = os.path.join(PROJECT_PATH, 'logging/metrics/registration_conversion.xls')
 
-completedTasksAnalytics(time_tuples, output_file)
+registrationConversionAnalytics(time_tuples, output_file)
