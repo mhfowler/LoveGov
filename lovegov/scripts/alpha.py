a2d231ba4b1e701113a8fa09830ce801061c9e92
Max Fowler <max_fowler@brown.edu>
2012-10-22 00:01:55 -0400

    workign on reparse answers

--- lovegov/scripts/alpha.py
+++ lovegov/scripts/alpha.py
@@ -51,7 +51,7 @@ def scriptReparseCongressAnswers():
     ]
     for x in specific_answer_files_relative:
         print enc("+SS+ creating responses based on file: " + x)
-        scriptCreateResponses(x)
+        scriptCreateResponses([x])
 
     print "finished script!"
 
