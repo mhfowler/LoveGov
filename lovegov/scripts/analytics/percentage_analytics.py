ab00eea6e2fd467ae3c015df0911a16ff18c5fc5
Max Fowler <max_fowler@brown.edu>
2012-10-25 16:47:57 -0400

    landmarks analytics

--- lovegov/scripts/analytics/percentage_analytics.py
+++ lovegov/scripts/analytics/percentage_analytics.py
@@ -5,12 +5,24 @@ if __name__ == "__main__":
     from lovegov.frontend.analytics import *
     from lovegov.frontend.views import *
 
+    time_tuples = []
+
+    start_date = datetime.datetime(year=2012, month=4, day=10, hour=6)
+    end_date = datetime.datetime(year=2012, month=7, day=4, hour=6)
+    time_tuples.append((start_date, end_date))
+
     start_date = datetime.datetime(year=2012, month=7, day=4, hour=6)
-    end_date = datetime.datetime.now()
+    end_date = datetime.datetime(year=2012, month=9, day=7, hour=6)
+    time_tuples.append((start_date, end_date))
 
-    delta = datetime.timedelta(days=7)
-    time_tuples = getTimeTuplesByDelta(start_date, end_date, delta)
+    start_date = datetime.datetime(year=2012, month=9, day=7, hour=6)
+    end_date = datetime.datetime(year=2012, month=10, day=25, hour=6)
+    time_tuples.append((start_date, end_date))
 
-    output_file = os.path.join(PROJECT_PATH, 'logging/metrics/percentage_analytics_weekly.xls')
+    start_date = datetime.datetime(year=2012, month=10, day=25, hour=6)
+    end_date = datetime.datetime(year=2012, month=11, day=6, hour=6)
+    time_tuples.append((start_date, end_date))
+
+    output_file = os.path.join(PROJECT_PATH, 'logging/metrics/percentage_analytics_landmarks.xls')
 
     percentageAnalytics(time_tuples=time_tuples, resolution=20, output_file=output_file)
