97efb587112a211d061509c437aa13e13ad43e69
Max Fowler <max_fowler@brown.edu>
2012-10-25 16:50:16 -0400

    the epoch

--- lovegov/scripts/analytics/percentage_analytics.py
+++ lovegov/scripts/analytics/percentage_analytics.py
@@ -8,10 +8,10 @@ if __name__ == "__main__":
     time_tuples = []
 
     start_date = datetime.datetime(year=2012, month=4, day=10, hour=6)
-    end_date = datetime.datetime(year=2012, month=7, day=5, hour=6)
+    end_date = datetime.datetime(year=2012, month=7, day=6, hour=6)
     time_tuples.append((start_date, end_date))
 
-    start_date = datetime.datetime(year=2012, month=7, day=5, hour=6)
+    start_date = datetime.datetime(year=2012, month=7, day=6, hour=6)
     end_date = datetime.datetime(year=2012, month=9, day=7, hour=6)
     time_tuples.append((start_date, end_date))
 
