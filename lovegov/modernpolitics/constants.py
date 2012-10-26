07321f7006ceb0f967e106780470128efe19459b
Jeremy Greenfield <jsgreenf@gmail.com>
2012-10-26 12:03:24 -0400

    share match results on facebook

--- lovegov/modernpolitics/constants.py
+++ lovegov/modernpolitics/constants.py
@@ -757,7 +757,8 @@ ACTIONS = [
     'matchWithGroups',
     'matchWithPeople',
     'getMatchCard',
-    'getMembersByUIDS'
+    'getMembersByUIDS',
+    'hideFacebookShareMatches',
 ]
 
 UNAUTHENTICATED_ACTIONS = [
