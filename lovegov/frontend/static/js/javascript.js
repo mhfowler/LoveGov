47bb734af3d3bdb59ff6b9958f44298d057dbdb7
Max Fowler <max_fowler@brown.edu>
2012-11-01 12:28:41 -0400

    master

--- lovegov/frontend/static/js/javascript.js
+++ lovegov/frontend/static/js/javascript.js
@@ -5158,7 +5158,7 @@ bind('.goto_sign_up', 'click', function(e) {
     var new_height = element.height();
     element.css("height", old_height);
     $('body').animate({
-        scrollTop: element.offset().top - 25
+        scrollTop: element.offset().top - 290
     }, 1000);
     element.animate({"height":new_height}, {"duration":1000,
         "complete":function(){
