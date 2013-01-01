7f1902bf4d39196277c59912d900d30b2ed503e1
Max Fowler <max_fowler@brown.edu>
2013-01-01 15:15:09 -0500

    retention

--- lovegov/scripts/analytics/percentage_analytics.py
+++ lovegov/scripts/analytics/percentage_analytics.py
@@ -8,6 +8,10 @@ if __name__ == "__main__":
     time_tuples = []
 
     start_date = datetime.datetime(year=2012, month=4, day=10, hour=6)
+    end_date = datetime.datetime(year=2012, month=11, day=6, hour=6)
+    time_tuples.append((start_date, end_date))
+
+    start_date = datetime.datetime(year=2012, month=4, day=10, hour=6)
     end_date = datetime.datetime(year=2012, month=7, day=10, hour=6)
     time_tuples.append((start_date, end_date))
 
@@ -23,6 +27,6 @@ if __name__ == "__main__":
     end_date = datetime.datetime(year=2012, month=11, day=6, hour=6)
     time_tuples.append((start_date, end_date))
 
-    output_file = os.path.join(PROJECT_PATH, 'logging/metrics/percentage_analytics_landmarks.xls')
+    output_file = os.path.join(PROJECT_PATH, 'logging/metrics/percentage_analytics_retention.xls')
 
     percentageAnalytics(time_tuples=time_tuples, resolution=20, output_file=output_file)
