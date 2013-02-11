f0659ce1a7c5e8b1df19ff07737fbde83527756f
Max Fowler <max_fowler@brown.edu>
2013-02-10 22:27:24 -0500

    yup

--- lovegov/test_file.py
+++ lovegov/test_file.py
@@ -3,5 +3,5 @@
 if __name__ == "__main__":
     from lovegov.modernpolitics.initialize import manuallyUpdate
     print "manually updating..."
-    xls_path = 'scripts/migration/ma.xls'
-    manuallyUpdate("MA", xls_path)
+    xls_path = 'scripts/migration/ct.xls'
+    manuallyUpdate("CT", xls_path)
